44a0e0a0eddb318b1ddea3a00da2b358433b084a
Ojasv <ojasv.rathore@gmail.com>
2024-02-29 17:41:16 +0530

    resume and github

--- src/nav_bar.tsx
+++ src/nav_bar.tsx
@@ -7,7 +7,7 @@ import NavBarButton from "./components/nav_bar_button_com";
 const NavBar = () => {
   const [show, showMenu] = useState(false);
   return (
-    <div className="bg-black sticky top-0 z-10">
+    <div className="sticky top-0 z-10 bg-black">
       {/* Nav Bar */}
       <div className="space-x-33 mx-20 flex h-20 justify-around max-sm:mx-0">
         <div className="flex flex-1">
@@ -26,7 +26,7 @@ const NavBar = () => {
           <img className="h-8 w-6" src={hamberger} alt="" />
         </button>
         {/* Nav Links */}
-        <div className="flex space-x-8 self-center pr-4 text-white max-md:hidden max-lg:ml-20">
+        <div className="flex space-x-8 self-center pr-4 text-white max-lg:ml-20 max-md:hidden">
           <NavBarButton id="1" name="Home" size="" />
           <NavBarButton id="2" name="About" size="" />
           <NavBarButton id="3" name="Project" size="" />
@@ -38,7 +38,7 @@ const NavBar = () => {
                  from-purple1 via-purple2 to-purple2 bg-[size:_200%] bg-[position:_0%_0%] px-4 font-bold text-white transition-all duration-500 hover:bg-[position:_100%_100%] "
         >
           <a
-            href="https://drive.google.com/file/d/19yXMK61rINxngp4xNSZKemH2oBnHh-Ok/view?usp=sharing"
+            href="https://drive.google.com/file/d/1DDJyx1e4hZTfJOsL5EJsQhXjRpm2fEvI/view?usp=sharing"
             className="self-center pr-2 "
           >
             <img src={resume} alt="" />
